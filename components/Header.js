e8d5490111360ace868c43c26d5593d972bf6303
Oriane <oriane.louis.pro@gmail.com>
2020-11-10 21:19:13 +0100

    feat: Link the home on the "logo"

--- components/Header.js
+++ components/Header.js
@@ -4,7 +4,9 @@ const Header = () => {
   return (
     <nav className="container py-5 fixed">
       <div className="mx-auto w-10/12 flex justify-between">
-        <div>LearnDaily</div>
+        <div>
+          <Link href="/">LearnDaily</Link>
+        </div>
         <ul className="flex">
           <li className="hover:underline">
             <Link href="/articles">Articles</Link>
