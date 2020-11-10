566d189335dfa58e7dfbdc2b4e93f2abc843b7fd
Oriane <oriane.louis.pro@gmail.com>
2020-11-10 22:41:06 +0100

    style: Not apply tailwind reset on markdown

--- components/BlogPost.js
+++ components/BlogPost.js
@@ -1,6 +1,6 @@
 export default function BlogPost({ children, meta }) {
   return (
-    <section>
+    <section className="prose lg:prose-xl">
       <h1>{meta.title}</h1>
       <div className="details">
         <span>{meta.date}</span>
