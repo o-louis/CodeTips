3cb0e8724aed3566f6d66772a3a6de1ed9e1c2b7
Oriane <oriane.louis.pro@gmail.com>
2020-12-04 12:40:44 -0500

    style: list style

--- components/BlogPost.js
+++ components/BlogPost.js
@@ -1,7 +1,7 @@
 export default function BlogPost({ children, meta }) {
   return (
     <section className="m-auto w-10/12 py-8 md:w-11/12">
-      <h1 className="text-3xl font-bold text-blue-600 pb-4">{meta.title}</h1>
+      <h1 className="text-3xl font-bold text-blue-600 pb-0">{meta.title}</h1>
       <article className="text-lg lg:text-xl text-blue-900 md:max-w-2xl">
         {children}
       </article>
