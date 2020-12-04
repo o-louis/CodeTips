dd4aa084a587747853c25df414f85fd7f9a568f6
Oriane <oriane.louis.pro@gmail.com>
2020-12-04 10:33:31 -0500

    feat: Add syntax highlighter for source code quote

--- components/BlogPost.js
+++ components/BlogPost.js
@@ -1,9 +1,8 @@
 export default function BlogPost({ children, meta }) {
   return (
     <section className="m-auto w-10/12 py-8 md:w-11/12 py-8 sm:text-4xl lg:text-5xl">
-      <h1 className="text-3xl font-bold text-blue-600 pb-1">{meta.title}</h1>
-
-      <article>{children}</article>
+      <h1 className="text-3xl font-bold text-blue-600 pb-4">{meta.title}</h1>
+      <article className="text-md lg:text-xl text-blue-900">{children}</article>
     </section>
   );
 }
