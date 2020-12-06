cbf99c9c48c2e35633fe394875f07c7d88a1b8ce
Oriane <oriane.louis.pro@gmail.com>
2020-12-06 16:57:33 -0500

    feat: Add the highlight tag for the code rendering

--- components/Code.js
+++ components/Code.js
@@ -1,15 +1,19 @@
-import { Light as SyntaxHighlighter } from "react-syntax-highlighter";
-import { hybrid } from "react-syntax-highlighter/dist/cjs/styles/hljs";
+import Highlight, { defaultProps } from "prism-react-renderer";
 
 export default function Code({ children }) {
   return (
-    <SyntaxHighlighter
-      language="javascript"
-      style={hybrid}
-      className="syntaxhighlighter font-sans rounded-md"
-      showLineNumbers
-    >
-      {children}
-    </SyntaxHighlighter>
+    <Highlight {...defaultProps} code={children} language="javascript">
+      {({ className, style, tokens, getLineProps, getTokenProps }) => (
+        <pre className={`${className} syntaxhighlighter`} style={style}>
+          {tokens.map((line, i) => (
+            <div {...getLineProps({ line, key: i })}>
+              {line.map((token, key) => (
+                <span {...getTokenProps({ token, key })} />
+              ))}
+            </div>
+          ))}
+        </pre>
+      )}
+    </Highlight>
   );
 }
