b53cb320a2d4a79438845940c784cddc30221705
Oriane <oriane.louis.pro@gmail.com>
2020-12-09 19:10:26 -0500

    feat: Get folder and its articles to add separators for each section

--- getAllPosts.js
+++ getAllPosts.js
@@ -1,10 +1,27 @@
 function importAll(r) {
-  return r.keys().map((fileName) => ({
-    link: fileName.substr(1).replace(/\/index\.mdx$/, ""),
-    module: r(fileName),
+  let folders = r.keys().map((post) => post.split("/")[1]);
+  folders = [...new Set(folders)].map((folder) => ({
+    name: folder,
+    posts: [],
   }));
+
+  const articles = r.keys().map((fileName) => {
+    return {
+      link: fileName.substr(1).replace(/\/index\.mdx$/, ""),
+      module: r(fileName),
+    };
+  });
+
+  for (let a = 0; a < folders.length; a++) {
+    for (let b = 0; b < articles.length; b++) {
+      if (articles[b].link.indexOf(folders[a].name) !== -1) {
+        folders[a].posts.push(articles[b]);
+      }
+    }
+  }
+  return folders;
 }
 
-export const posts = importAll(
+export const folders = importAll(
   require.context("./pages/posts/", true, /\.mdx$/)
 );
