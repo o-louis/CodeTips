34977eb152786059e76a8db82b21229697ca3290
Oriane <oriane.louis.pro@gmail.com>
2020-12-11 21:05:11 -0500

    feat: Create icon to enable dark mode

--- components/Header.js
+++ components/Header.js
@@ -8,6 +8,20 @@ const Header = ({ openMenu }) => {
           <Link href="/">CodeTips</Link>
         </div>
 
+        <svg
+          class="w-6 h-6 hidden md:inline-block md:cursor-pointer md:text-blue-300 md:hover:text-blue-600"
+          fill="none"
+          stroke="currentColor"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
+          ></path>
+        </svg>
         <div
           className="rounded-full p-1 cursor-pointer hover:bg-gray-50 md:hidden"
           onClick={openMenu}
