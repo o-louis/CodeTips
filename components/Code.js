1c1aba84f4e89848652bf6c5092a53658b042986
Oriane <oriane.louis.pro@gmail.com>
2020-12-20 19:42:20 -0500

    style: Change theme of the source code tag

--- components/Code.js
+++ components/Code.js
@@ -1,16 +1,19 @@
 import Highlight, { defaultProps } from "prism-react-renderer";
-import nightOwl from "prism-react-renderer/themes/nightOwl";
+import dracula from "prism-react-renderer/themes/dracula";
 
 export default function Code({ children }) {
   return (
     <Highlight
       {...defaultProps}
-      theme={nightOwl}
+      theme={dracula}
       code={children}
       language="javascript"
     >
       {({ className, style, tokens, getLineProps, getTokenProps }) => (
-        <pre className={`${className} syntaxhighlighter rounded-lg`} style={style}>
+        <pre
+          className={`${className} syntaxhighlighter rounded-lg`}
+          style={style}
+        >
           {tokens.map((line, i) => (
             <div {...getLineProps({ line, key: i })}>
               {line.map((token, key) => (
