b699977b4644ea5a8f1d64cd4ba2f5435bcf223d
Oriane <oriane.louis.pro@gmail.com>
2021-01-24 22:38:41 -0500

    refactor: Postcss config

--- postcss.config.js
+++ postcss.config.js
@@ -1,19 +1,16 @@
+const purgecss = [
+  "@fullhuman/postcss-purgecss",
+  {
+    content: ["./components/**/*.js", "./pages/**/*.js"],
+    defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+  },
+];
+
 module.exports = {
   plugins: [
+    "postcss-import",
     "tailwindcss",
-    process.env.NODE_ENV === "production"
-      ? [
-          "@fullhuman/postcss-purgecss",
-          {
-            content: [
-              "./pages/**/*.{js,jsx,ts,tsx}",
-              "./components/**/*.{js,jsx,ts,tsx}",
-            ],
-            defaultExtractor: (content) =>
-              content.match(/[\w-/:]+(?<!:)/g) || [],
-          },
-        ]
-      : undefined,
     "autoprefixer",
+    ...(process.env.NODE_ENV === "production" ? [purgecss] : []),
   ],
 };
