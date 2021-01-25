0495f0dc5d85821314e804f044d1f7c3086594a4
Oriane <oriane.louis.pro@gmail.com>
2021-01-24 22:32:18 -0500

    chore: Fix paths of the folder thats should be purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,37 +1,28 @@
 module.exports = {
-  purge: [
-    "./src/components/**/*.{js,ts,jsx,tsx}",
-    "./src/pages/**/*.{js,ts,jsx,tsx}",
-  ],
+  purge: ["./components/**/*.{js,ts,jsx,tsx}", "./pages/**/*.{js,ts,jsx,tsx}"],
   darkMode: "class",
   theme: {
-    
     extend: {
       maxWidth: {
         xs: "15rem",
       },
       colors: {
-      blue: {
-        100: "#DBEAFE",
-        200: "#BFDBFE",
-        300: "#93C5FD",
-        400: "#60A5FA",
-        500: "#3B82F6",
-        600: "#2563EB",
-        700: "#1D4ED8",
-        800: "#1E40AF",
-        900: "#1E3A8A",
+        blue: {
+          100: "#DBEAFE",
+          200: "#BFDBFE",
+          300: "#93C5FD",
+          400: "#60A5FA",
+          500: "#3B82F6",
+          600: "#2563EB",
+          700: "#1D4ED8",
+          800: "#1E40AF",
+          900: "#1E3A8A",
+        },
       },
-    },
     },
   },
   variants: {
-    margin: ['responsive', 'hover'],
-    backgroundColor: [
-      "responsive",
-      "hover",
-      "focus",
-      "dark",
-    ],
+    margin: ["responsive", "hover"],
+    backgroundColor: ["responsive", "hover", "focus", "dark"],
   },
 };
