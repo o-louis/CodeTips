5a4c014637515c2a5d6764df7bb4cb442d970285
o-louis <oriane.louis.pro@gmail.com>
2022-01-07 13:01:30 -0500

    chore: Test config without fullhuman and cssnano plugins

--- postcss.config.js
+++ postcss.config.js
@@ -1,27 +1,8 @@
 /* eslint-disable no-undef */
 const plugins = {
-  'postcss-import': {},
   'tailwindcss': {},
   'autoprefixer': {},
 };
-
-if (process.env.NODE_ENV === 'production') {
-  plugins['@fullhuman/postcss-purgecss'] = {
-    content: [
-      'pages/**/*.jsx',
-      'components/**/*.jsx',
-    ],
-    defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
-  };
-
-  plugins['cssnano'] = {
-    preset: ['default', {
-      discardComments: {
-        removeAll: true,
-      },
-    }],
-  };
-}
 
 module.exports = {
   plugins,
