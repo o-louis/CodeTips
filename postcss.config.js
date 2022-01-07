342ea07f7c5c43e4a6965cae4360d14a5976b53b
o-louis <oriane.louis.pro@gmail.com>
2022-01-07 13:24:21 -0500

    chore: Add safelist property to purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,18 @@
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
+const purgecss = [
+  '@fullhuman/postcss-purgecss',
+  {
+    content: ['./components/**/*.jsx', './pages/**/*.jsx'],
+    safelist: ['html', 'body'],
+    defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
   },
+];
+
+module.exports = {
+  plugins: [
+    'postcss-import',
+    'tailwindcss',
+    'autoprefixer',
+    // eslint-disable-next-line no-undef
+    ...(process.env.NODE_ENV === 'production' ? [purgecss, 'cssnano'] : []),
+  ],
 };
